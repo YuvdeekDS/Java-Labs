63a71f16c2225cd1ad69ac2c9285f1a8b035a736
MadhanaGopal <madhan@yuvdeekdigits.in>
2023-01-07 16:01:39 +0530

    java coursework completed and added junit test case

--- Coursework/src/main/java/coursework_question1/Advert.java
+++ Coursework/src/main/java/coursework_question1/Advert.java
@@ -9,25 +9,37 @@ public class Advert {
 	private List<Offer> offers;
 
 	public Advert(Car car) {
+		//accepts only objects of Car class
+		if(car == null) {
+		throw new IllegalArgumentException();	
+		}else {
 		this.car = car;
 		this.offers = new ArrayList<Offer>();
+		}
 	}
 	
 	public boolean placeOffer(User buyer,double value) {
-		
-		Offer newOffer = new Offer(buyer,value);
-		return this.offers.add(newOffer);
+		//accepts only objects of User class and adds the offer to list 
+		if(buyer == null) {
+		throw new IllegalArgumentException();
+		}else {			
+			Offer newOffer = new Offer(buyer,value);
+			return this.offers.add(newOffer);
+		}
 	}
 	
 	public Car getCar() {
+		//returns the car of the Advert object
 		return this.car;
 	}
 	
 	public List<Offer> getOffers(){
+		//returns list of offers
 		return offers;
 	}
 	
 	public Offer getHighestOffer() {
+		//returns highest offer among the offers placed to the Advert.
 		Offer highestOffer = offers.stream().max(Comparator.comparingDouble(Offer::getValue)).get();
 		return highestOffer;
 	}
